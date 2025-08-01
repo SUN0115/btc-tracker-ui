b3d654562cba8e495549623232b2c30be49590ef
ian_sun <didi900115@gmail.com>
2025-08-01 10:54:13 +0800

    修改ap網址

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -2,8 +2,13 @@
 import axios, { type AxiosResponse } from 'axios';
 import type { RespondObj, MainDashboardDto, DailyAverageDto } from '@/types/api';
 
+// const apiClient = axios.create({
+//   baseURL: 'https://btc-tracker-api-rj02.onrender.com/api', // 後端 API URL
+// });
 const apiClient = axios.create({
-  baseURL: 'https://btc-tracker-api-rj02.onrender.com/api', // 後端 API URL
+  // 這裡不能是 localhost！
+  // 必須是你 Render 部署成功後的公開網址
+  baseURL: 'https://btc-tracker-api-rj02.onrender.com/api', 
 });
 
 // 為每個方法加上明確的回傳型別 Promise<AxiosResponse<...>>
